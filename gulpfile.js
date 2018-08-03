846ac457643e89dea2db35801dca38ac0e17eaea
Mauriverti da Silva Junior <mauriverti@consulfarmasaude.com.br>
2018-08-03 11:39:02 -0300

    Reorganized gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,28 @@
-let gulp =        require('gulp');                  // automates tasks
-let pug =         require('gulp-pug');              // Compiles pug files
-let clean =       require('gulp-clean');            // Removes folders
-let sass =        require('gulp-sass');             // Compiles sass files
-let browserify =  require('browserify');            // Enables the import/require function in browsers
-let source =      require('vinyl-source-stream');   // Avoids the needs of browserify-based libs and their overhead over browserify lib
-let tsify =       require('tsify');                 // Browserify plugin for compiling TypeScript
-let watchify =    require("watchify");              // Watch mode for browserify builds
-let gutil =       require('gulp-util');             // Utility functions for gulp plugins
-let buffer =      require('vinyl-buffer');          // Converts streaming vinyl files to use buffers
-let sourcemaps =  require('gulp-sourcemaps');       // Creates sourcemaps to uglyfied files
-let uglify =      require('gulp-uglify');           // Minifies files
-let webserver =   require('gulp-webserver');        // Webserver to test code in developer environment
 let argv =        require('yargs').argv;            // Helps to get adicional paramethers from terminal
+let browserify =  require('browserify');            // Enables the import/require function in browsers
+let buffer =      require('vinyl-buffer');          // Converts streaming vinyl files to use buffers
+let clean =       require('gulp-clean');            // Removes folders
+let conf =        require('./package.json');        // Loads project paramethers
+let gulp =        require('gulp');                  // automates tasks
+let gutil =       require('gulp-util');             // Utility functions for gulp plugins
+let pug =         require('gulp-pug');              // Compiles pug files
+let sass =        require('gulp-sass');             // Compiles sass files
+let source =      require('vinyl-source-stream');   // Avoids the needs of browserify-based libs and their overhead over browserify lib
+let sourcemaps =  require('gulp-sourcemaps');       // Creates sourcemaps to uglyfied files
+let tsify =       require('tsify');                 // Browserify plugin for compiling TypeScript
+let uglify =      require('gulp-uglify');           // Minifies files
+let watchify =    require("watchify");              // Watch mode for browserify builds
+let webserver =   require('gulp-webserver');        // Webserver to test code in developer environment
 
-let conf = {
+Object.assign( conf, {
   appFolder: `./app/`,
-  buildFolder: './dist/app/',
+  buildFolder: './dist/',
+  port:       argv.port    || 4500,
   serverIP:   argv.apiHost || 'localhost',
   serverPort: argv.apiPort || 8080,
   serverName: argv.apiName || 'system',
   prod: argv.prod,
-}
+});
 
 gulp.task('clean', () =>
   gulp.src(conf.buildFolder)
@@ -61,19 +63,19 @@ function bundle () {
   .pipe(gulp.dest(conf.buildFolder))
 }
 
-gulp.task('build', ['pug-compile', 'sass-compile', 'watch'], bundle);
+gulp.task('compile', ['pug-compile', 'sass-compile', 'watch', 'preview'], bundle);
 
-gulp.task('webserver', () => {
+gulp.task('preview', () => {
   gulp.src(conf.buildFolder)
     .pipe(webserver({
       livereload: true,
       host: '0.0.0.0',
-      port: 4500,
+      port: conf.port,
       https: false,
       directoryListening: true,
       proxies: [{
-        source: '/api',
-        target: `http://${conf.serverIP}:${conf.serverPort}/${conf.serverName}`
+        source: '/api/',
+        target: `http://${conf.serverIP}:${conf.serverPort}/${conf.serverName}/`
       }]
     }))
 });
@@ -84,4 +86,4 @@ gulp.task('watch', () => {
 });
 
 
-gulp.task( 'default', ['build', 'webserver']);
+gulp.task( 'default', ['compile']);
